0379bb7a5c2c74c2d8715a9470c2220cb00bb924
Mahsa Keikha <mahsa.keikha@gmail.com>
2026-09-21 21:01:14 -0700

    Test primitive factor pressure partition

--- tests/test_certificate_sensitivity.py
+++ tests/test_certificate_sensitivity.py
@@ -17,3 +17,14 @@ def test_equal_fraction_tightening_gain_matches_target_burden():
     y=counterfactual_tightening(empirical_margin=1.,tightening_fraction=.25,**KW)
     assert np.isclose(y["local_gain"],.25*x.local_burden)
     assert np.isclose(y["transport_gain"],.25*x.transport_burden)
+
+
+def test_primitive_factor_pressure_partitions_pre_score_error():
+    from observer_math.certificate_sensitivity import primitive_factor_pressure
+    x=primitive_factor_pressure(covariance_relative_error=.01,
+        leader_local_factors=LOCAL,leader_transport_factors=TRANS,
+        competitor_local_factors=LOCAL,competitor_transport_factors=TRANS,
+        subset_size=3,ambient_size=7)
+    assert x["valid"]
+    assert np.isclose(sum(x["fractions"].values()),1.)
+    assert np.isclose(sum(x["pressure"].values()),x["total_primitive_pressure"])
